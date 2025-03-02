b4863c31ea9abc685bdb88c98e0f06839905892a
jrrHernandez <123601048+jrrHernandez@users.noreply.github.com>
2025-03-02 10:56:44 -0500

    Create CodigoT2.R

---
+++ CodigoT2.R
@@ -0,0 +1,22 @@
+###############################################################################
+######Taller 2 practico econometria 2##########################################
+###################2025-1######################################################
+###Profesora: Juliana Helo ####################################################
+#Integrantes
+# Jose Ricardo Ricardo Hernandez - 202113889
+#
+#
+###############################################################################
+#PUNTO 1#
+#cargamos la base de de datos "BaseP1.dta"
+require(pacman)
+p_load(rio)
+setwd("C:/Users/richa/OneDrive - Universidad de los Andes/Universidad/octavo/Econometria 2/taller 2/taller2 practico/bases"
+)
+base1 <- import("C:/Users/richa/OneDrive - Universidad de los Andes/Universidad/octavo/Econometria 2/taller 2/taller2 practico/bases/BaseP1.dta")
+
+
+
+  
+  
+  
